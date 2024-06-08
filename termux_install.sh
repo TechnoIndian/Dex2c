2b2059990a148c8f1e3f2cc5751d6dcd4d697aea
TechnoIndian <133761719+TechnoIndian@users.noreply.github.com>
2024-06-08 14:49:36 +0530

    termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -91,7 +91,7 @@ echo "${green}━━━ Setting up apktool ━━━${nocolor}"
 if [ -f "$PREFIX/bin/apktool.jar" ]; then
   echo "${blue}apktool is already installed${nocolor}"
 else
-  sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.1.jar -O $PREFIX/bin/apktool.jar'
+  sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.3.jar -O $PREFIX/bin/apktool.jar'
   
   chmod +r $PREFIX/bin/apktool.jar
   
@@ -112,7 +112,7 @@ if [ -f "$HOME/dex2c/tools/apktool.jar" ]; then
   rm $HOME/dex2c/tools/apktool.jar
   cp $PREFIX/bin/apktool.jar $HOME/dex2c/tools/apktool.jar
 else
-sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.1.jar -O $HOME/dex2c/tools/apktool.jar'
+sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.9.3.jar -O $HOME/dex2c/tools/apktool.jar'
 fi
 
 cd ~/dex2c
